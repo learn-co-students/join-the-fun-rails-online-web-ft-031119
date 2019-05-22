a036cef763c9b8ad02e921957469ccf744d0d9c5
Jon <jhharwood88@gmail.com>
2019-05-22 15:29:08 -0400

    Done.

--- db/migrate/20190522184735_add_taxi_id_to_passengers.rb
+++
@@ -1,5 +0,0 @@
-class AddTaxiIdToPassengers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :passengers, :taxi_id, :integer
-  end
-end
--- db/migrate/20190522184828_add_passengers_id_to_taxis.rb
+++
@@ -1,5 +0,0 @@
-class AddPassengersIdToTaxis < ActiveRecord::Migration[5.0]
-  def change
-    add_column :taxis, :passenger_id, :integer
-  end
-end
